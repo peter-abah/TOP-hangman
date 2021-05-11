d6ee8ded93469b6c046c64f359d7292237ee38f3
Peter Abah <peterabah.ob@gmail.com>
2021-05-11 19:29:54 +0100

    add play method to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -7,5 +7,22 @@ class Game
     @guessed = Set[]
     @letters = Set[]
     @won = false
+    @incorrect = 0
+  end
+
+  def play
+    until @won && @incorrect > 7
+      puts in_game_message()
+      letter = gets.chomp.downcase
+
+      if letter == @hidden_word
+        @won = true
+      else
+        hidden_letters.include?(letter) ? @letters.add(letter) : @incorrect += 1
+        guessed.add(letter)
+      end
+      @won = true if letters == hidden_letters
+    end
+    puts end_game_message()
   end
 end
