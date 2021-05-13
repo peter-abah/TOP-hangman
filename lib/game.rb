3b9dcd473e638ba0bc97806a9ece9e2ba0fd85c6
Peter Abah <peterabah.ob@gmail.com>
2021-05-13 09:49:12 +0100

    add in_game_message method to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
 #frozen_string_literal: true
 
+require 'set'
+
 class Game
   def initialize(word)
     @hidden_word = word
@@ -12,7 +14,7 @@ class Game
 
   def play
     until @won && @incorrect > 7
-      puts in_game_message()
+      puts in_game_message
       letter = gets.chomp.downcase
 
       if letter == @hidden_word
@@ -23,6 +25,17 @@ class Game
       end
       @won = true if letters == hidden_letters
     end
-    puts end_game_message()
+    puts end_game_message
+  end
+
+  def in_game_message
+    dashed = []
+    @hidden_word.each_char do |char|
+      @letters.include?(char) ? dashed.push(char) : dashed.push('-')
+    end
+    "Guess a letter\n" \
+      "#{dashed.join(' ')}\n" \
+      "Incorrect : #{@incorrect}\n" \
+      "Guessed Letters: #{@guessed.to_a.join(', ')}"
   end
 end
