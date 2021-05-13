ebf9e69d22eb87516772ad5c5776c9287154cbec
Peter Abah <peterabah.ob@gmail.com>
2021-05-13 13:28:43 +0100

    add display_welcome_message method to main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -5,9 +5,17 @@ require_relative 'game'
 def main
   words = load_words
 
-  show_welcome_message
+  display_welcome_message
   choice = gets.chomp.downcase
 
   game = choice == 'load' ? load_game : Game.new(words.sample)
   game.play
+end
+
+def display_welcome_message
+  welcome_message =  
+    "Welcome to hangman\n"\
+    'Enter load to continue a saved game or '\
+    'press Enter to start a new game'
+  puts welcome_message
 end
